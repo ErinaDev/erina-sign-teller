9aa1e55f00900c7753bb74ffba3b08ea1ab3f0bf
Devsaider <sebasmoraresu@gmail.com>
2021-01-12 18:51:26 -0500

    fix months

--- utils/index.js
+++ utils/index.js
@@ -5,14 +5,25 @@ module.exports = {
    */
   getDateDetails(day, month, year = 2000) {
     const months = [
-      'January', 'February', 'March', 'April',
-      'May', 'June', 'July', 'August',
-      'September', 'October', 'November', 'December'
+      "Enero",
+      "Febrero",
+      "Marzo",
+      "Abril",
+      "Mayo",
+      "Junio",
+      "Julio",
+      "Agosto",
+      "Septiembre",
+      "Octubre",
+      "Noviembre",
+      "Diciembre",
     ];
 
     const date = new Date(year, month - 1, day); // month - 1 because 0 = Jan and Dec = 11
     const monthNum = date.getMonth();
-    const monthFull = months.find(month => months.indexOf(month) === monthNum);
+    const monthFull = months.find(
+      (month) => months.indexOf(month) === monthNum
+    );
     return [monthFull, day];
-  }
-}
+  },
+};
